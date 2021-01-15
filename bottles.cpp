655015942c5cef8a1af57c5c11414aa60025a271
Leonardo-Malcotti <57906466+Leonardo-Malcotti@users.noreply.github.com>
2021-01-15 01:04:08 +0100

    Add files via upload

---
+++ bottles.cpp
@@ -0,0 +1,26 @@
+
+
+#include <string>//#####
+#include <iostream>//###
+using   namespace  std;//#
+typedef string st;//#######-----
+int  main(){////////////////-----
+int ber = 99;//#########      \\\\
+//Leonardo #############        ###  
+//Malcotti##############        ###
+st b=" bottles";//######        ###
+st o=" of beer";//######        ###
+st where=" on the wall";        //#
+st what="Take one down";        //#
+st ps=",pass it around";        //#
+while (ber != 0+0){//###        ### 
+cout<<ber<<b<<o;//######        ###
+cout<<where; cout<<endl;        //#
+cout <<ber;cout<<b<<o;//        ###
+//99 Bottles of beers###       ####
+//written in beer shape#      ####
+//with basic c++########     ////  
+cout << endl;cout<<what<<ps;//
+cout<<endl;ber--;cout<<ber<<b;
+cout<<o<<where<<endl<<endl;
+};return 0;};/////2021/1/15
